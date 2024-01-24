ec04ca419f29dd5f30345981aea07eb83392ce09
Антон <61877084+AntonPrNone@users.noreply.github.com>
2024-01-24 16:55:22 +0300

    Финалочка

--- PracWinForms2.2/Form1.Designer.cs
+++ PracWinForms2.2/Form1.Designer.cs
@@ -28,13 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.AndreySwimming_TextBox = new System.Windows.Forms.TextBox();
+            this.EgorSwimming_TextBox = new System.Windows.Forms.TextBox();
+            this.MichaelSwimming_TextBox = new System.Windows.Forms.TextBox();
+            this.AndreyBicycle_TextBox = new System.Windows.Forms.TextBox();
+            this.EgorBicycle_TextBox = new System.Windows.Forms.TextBox();
+            this.MichaelBicycle_TextBox = new System.Windows.Forms.TextBox();
+            this.AndreyRunning_TextBox = new System.Windows.Forms.TextBox();
+            this.EgorRunning_TextBox = new System.Windows.Forms.TextBox();
+            this.MichaelRunning_TextBox = new System.Windows.Forms.TextBox();
+            this.AndreyTime_TextBox = new System.Windows.Forms.TextBox();
+            this.EgorTime_TextBox = new System.Windows.Forms.TextBox();
+            this.MichaelTime_TextBox = new System.Windows.Forms.TextBox();
+            this.Time_Button = new System.Windows.Forms.Button();
+            this.Champ_Button = new System.Windows.Forms.Button();
+            this.Champ_TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(269, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Соревнование по триатлону";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(40, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Скорость/Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(40, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Плавание (км/ч)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(40, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Велосипед (км/ч)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(40, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Бег (км/ч)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(40, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Общее время (мин)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(247, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Андрей";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(446, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Егор";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(652, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Михаил";
+            // 
+            // AndreySwimming_TextBox
+            // 
+            this.AndreySwimming_TextBox.Location = new System.Drawing.Point(251, 145);
+            this.AndreySwimming_TextBox.Name = "AndreySwimming_TextBox";
+            this.AndreySwimming_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.AndreySwimming_TextBox.TabIndex = 9;
+            // 
+            // EgorSwimming_TextBox
+            // 
+            this.EgorSwimming_TextBox.Location = new System.Drawing.Point(450, 147);
+            this.EgorSwimming_TextBox.Name = "EgorSwimming_TextBox";
+            this.EgorSwimming_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.EgorSwimming_TextBox.TabIndex = 10;
+            // 
+            // MichaelSwimming_TextBox
+            // 
+            this.MichaelSwimming_TextBox.Location = new System.Drawing.Point(656, 147);
+            this.MichaelSwimming_TextBox.Name = "MichaelSwimming_TextBox";
+            this.MichaelSwimming_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.MichaelSwimming_TextBox.TabIndex = 11;
+            // 
+            // AndreyBicycle_TextBox
+            // 
+            this.AndreyBicycle_TextBox.Location = new System.Drawing.Point(251, 184);
+            this.AndreyBicycle_TextBox.Name = "AndreyBicycle_TextBox";
+            this.AndreyBicycle_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.AndreyBicycle_TextBox.TabIndex = 12;
+            // 
+            // EgorBicycle_TextBox
+            // 
+            this.EgorBicycle_TextBox.Location = new System.Drawing.Point(450, 186);
+            this.EgorBicycle_TextBox.Name = "EgorBicycle_TextBox";
+            this.EgorBicycle_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.EgorBicycle_TextBox.TabIndex = 13;
+            // 
+            // MichaelBicycle_TextBox
+            // 
+            this.MichaelBicycle_TextBox.Location = new System.Drawing.Point(656, 186);
+            this.MichaelBicycle_TextBox.Name = "MichaelBicycle_TextBox";
+            this.MichaelBicycle_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.MichaelBicycle_TextBox.TabIndex = 14;
+            // 
+            // AndreyRunning_TextBox
+            // 
+            this.AndreyRunning_TextBox.Location = new System.Drawing.Point(251, 223);
+            this.AndreyRunning_TextBox.Name = "AndreyRunning_TextBox";
+            this.AndreyRunning_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.AndreyRunning_TextBox.TabIndex = 15;
+            // 
+            // EgorRunning_TextBox
+            // 
+            this.EgorRunning_TextBox.Location = new System.Drawing.Point(450, 225);
+            this.EgorRunning_TextBox.Name = "EgorRunning_TextBox";
+            this.EgorRunning_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.EgorRunning_TextBox.TabIndex = 16;
+            // 
+            // MichaelRunning_TextBox
+            // 
+            this.MichaelRunning_TextBox.Location = new System.Drawing.Point(656, 225);
+            this.MichaelRunning_TextBox.Name = "MichaelRunning_TextBox";
+            this.MichaelRunning_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.MichaelRunning_TextBox.TabIndex = 17;
+            // 
+            // AndreyTime_TextBox
+            // 
+            this.AndreyTime_TextBox.Location = new System.Drawing.Point(251, 264);
+            this.AndreyTime_TextBox.Name = "AndreyTime_TextBox";
+            this.AndreyTime_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.AndreyTime_TextBox.TabIndex = 18;
+            // 
+            // EgorTime_TextBox
+            // 
+            this.EgorTime_TextBox.Location = new System.Drawing.Point(450, 264);
+            this.EgorTime_TextBox.Name = "EgorTime_TextBox";
+            this.EgorTime_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.EgorTime_TextBox.TabIndex = 19;
+            // 
+            // MichaelTime_TextBox
+            // 
+            this.MichaelTime_TextBox.Location = new System.Drawing.Point(656, 266);
+            this.MichaelTime_TextBox.Name = "MichaelTime_TextBox";
+            this.MichaelTime_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.MichaelTime_TextBox.TabIndex = 20;
+            // 
+            // Time_Button
+            // 
+            this.Time_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Time_Button.Location = new System.Drawing.Point(350, 319);
+            this.Time_Button.Name = "Time_Button";
+            this.Time_Button.Size = new System.Drawing.Size(100, 34);
+            this.Time_Button.TabIndex = 21;
+            this.Time_Button.Text = "Время";
+            this.Time_Button.UseVisualStyleBackColor = true;
+            this.Time_Button.Click += new System.EventHandler(this.Time_Button_Click);
+            // 
+            // Champ_Button
+            // 
+            this.Champ_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Champ_Button.Location = new System.Drawing.Point(350, 369);
+            this.Champ_Button.Name = "Champ_Button";
+            this.Champ_Button.Size = new System.Drawing.Size(100, 34);
+            this.Champ_Button.TabIndex = 22;
+            this.Champ_Button.Text = "Чемпион";
+            this.Champ_Button.UseVisualStyleBackColor = true;
+            this.Champ_Button.Click += new System.EventHandler(this.Champ_Button_Click);
+            // 
+            // Champ_TextBox
+            // 
+            this.Champ_TextBox.Location = new System.Drawing.Point(350, 421);
+            this.Champ_TextBox.Name = "Champ_TextBox";
+            this.Champ_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Champ_TextBox.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.Controls.Add(this.Champ_TextBox);
+            this.Controls.Add(this.Champ_Button);
+            this.Controls.Add(this.Time_Button);
+            this.Controls.Add(this.MichaelTime_TextBox);
+            this.Controls.Add(this.EgorTime_TextBox);
+            this.Controls.Add(this.AndreyTime_TextBox);
+            this.Controls.Add(this.MichaelRunning_TextBox);
+            this.Controls.Add(this.EgorRunning_TextBox);
+            this.Controls.Add(this.AndreyRunning_TextBox);
+            this.Controls.Add(this.MichaelBicycle_TextBox);
+            this.Controls.Add(this.EgorBicycle_TextBox);
+            this.Controls.Add(this.AndreyBicycle_TextBox);
+            this.Controls.Add(this.MichaelSwimming_TextBox);
+            this.Controls.Add(this.EgorSwimming_TextBox);
+            this.Controls.Add(this.AndreySwimming_TextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox AndreySwimming_TextBox;
+        private System.Windows.Forms.TextBox EgorSwimming_TextBox;
+        private System.Windows.Forms.TextBox MichaelSwimming_TextBox;
+        private System.Windows.Forms.TextBox AndreyBicycle_TextBox;
+        private System.Windows.Forms.TextBox EgorBicycle_TextBox;
+        private System.Windows.Forms.TextBox MichaelBicycle_TextBox;
+        private System.Windows.Forms.TextBox AndreyRunning_TextBox;
+        private System.Windows.Forms.TextBox EgorRunning_TextBox;
+        private System.Windows.Forms.TextBox MichaelRunning_TextBox;
+        private System.Windows.Forms.TextBox AndreyTime_TextBox;
+        private System.Windows.Forms.TextBox EgorTime_TextBox;
+        private System.Windows.Forms.TextBox MichaelTime_TextBox;
+        private System.Windows.Forms.Button Time_Button;
+        private System.Windows.Forms.Button Champ_Button;
+        private System.Windows.Forms.TextBox Champ_TextBox;
     }
 }
 
--- PracWinForms2.2/Form1.cs
+++ PracWinForms2.2/Form1.cs
@@ -1,20 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PracWinForms2._2
 {
     public partial class Form1 : Form
     {
+        const double Swim = 1.5;
+        const double Bicycle = 40;
+        const double Running = 10;
+
+        double AndreyTime;
+        double EgorTime;
+        double MichaelTime;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Time_Button_Click(object sender, EventArgs e)
+        {
+            TimePeople();
+
+            AndreyTime_TextBox.Text = (AndreyTime * 60).ToString();
+            EgorTime_TextBox.Text = (EgorTime * 60).ToString();
+            MichaelTime_TextBox.Text = (MichaelTime * 60).ToString();
+        }
+
+        private void Champ_Button_Click(object sender, EventArgs e)
+        {
+            TimePeople();
+
+            string winner;
+            double minTime = Math.Min(AndreyTime, Math.Min(EgorTime, MichaelTime));
+
+            List<string> winners = new List<string>();
+            if (minTime == AndreyTime)
+                winners.Add("Андрей");
+            if (minTime == EgorTime)
+                winners.Add("Егор");
+            if (minTime == MichaelTime)
+                winners.Add("Михаил");
+
+            if (winners.Count == 1)
+            {
+                winner = winners[0];
+            }
+
+            else if (winners.Count > 1)
+            {
+                winner = "Ничья";
+            }
+
+            else
+            {
+                winner = "Ошибка";
+            }
+
+            Champ_TextBox.Text = winner;
+        }
+
+        private void TimePeople()
+        {
+            try
+            {
+                AndreyTime = Swim / double.Parse(AndreySwimming_TextBox.Text) +
+                             Bicycle / double.Parse(AndreyBicycle_TextBox.Text) +
+                             Running / double.Parse(AndreyRunning_TextBox.Text);
+
+                EgorTime = Swim / double.Parse(EgorSwimming_TextBox.Text) +
+                           Bicycle / double.Parse(EgorBicycle_TextBox.Text) +
+                           Running / double.Parse(EgorRunning_TextBox.Text);
+
+                MichaelTime = Swim / double.Parse(MichaelSwimming_TextBox.Text) +
+                              Bicycle / double.Parse(MichaelBicycle_TextBox.Text) +
+                              Running / double.Parse(MichaelRunning_TextBox.Text);
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка ввода. Пожалуйста, введите корректные числовые значения.",
+                                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                AndreyTime = EgorTime = MichaelTime = 0;
+            }
         }
     }
 }
